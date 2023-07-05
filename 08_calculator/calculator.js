293d765e3de4ffa3e055dbfc70cac17b86f8a405
eric-merritt <ericm2009@gmail.com>
2023-07-05 15:41:26 -0600

    completed exercises     removed breaks on test functions     completed exercises     removed breaks on test functions     began working on exercise. not finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,39 @@
-const add = function() {
+const add = function(num1,num2) {
+  return parseInt(num1) + parseInt(num2);
+};
+
+const subtract = function(num1,num2) {
+  return parseInt(num1) - parseInt(num2);
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+
+  return array.reduce((total,current) => total + current, 0);
+  
+  }
+
+;
+
+const multiply = function(...args) {
+    let product = 1;
+    for (i = 0; i < args.length; i++){
+      product *= args[i];
+    }
+    return product;
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) return 1;
+    let product = 1;
+  for (let i = n; i > 0; i--){
+    product *= i;
+  } return product;
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let forwardStr = string.toLowerCase().replace(/[^a-zA-Z0-9]/g,'');
 
-};
+    return forwardStr.split('').reverse().join('') == forwardStr;
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
